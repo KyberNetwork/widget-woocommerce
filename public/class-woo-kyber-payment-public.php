3f797936bc3f03962232e1c7d7026d9250381b09
Hoàng Hà <halink0803@gmail.com>
2018-12-04 14:50:47 +0700

    update css version

--- public/class-woo-kyber-payment-public.php
+++ public/class-woo-kyber-payment-public.php
@@ -72,7 +72,7 @@ class Woo_Kyber_Payment_Public {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/woo-kyber-payment-public.css', array(), "0.0.2", 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/woo-kyber-payment-public.css', array(), "0.0.3", 'all' );
 		$kyber_settings= get_option( 'woocommerce_kyber_settings', 1 );
 		$version = $kyber_settings['version'];
 		$version = $version ? $version : "v0.2";
